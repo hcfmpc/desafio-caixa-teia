1feb4c7d11684ad7772827d1ffa0f5957b349b7e
Elias de Almeida Jacome Filho <c079795@corp.caixa.gov.br>
2024-03-21 05:22:45 -0300

    refator: carregamento dos modulos no componente principal

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { LayoutModule } from '../core/layout/layout.module';
+import { HttpService } from '../core/http/http.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'desafio-caixa-teia';
+  constructor(private httpService: HttpService) {}
 }
