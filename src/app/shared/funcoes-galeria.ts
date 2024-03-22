3c57269827860bafff423590e694e6dda2e31af7
Elias de Almeida Jacome Filho <c079795@corp.caixa.gov.br>
2024-03-21 22:58:20 -0300

    refator(galeria): ajuste filtro para não atuar sobre a palavra Album

--- src/app/shared/funcoes-galeria.ts
+++ src/app/shared/funcoes-galeria.ts
@@ -45,8 +45,19 @@ export class FuncoesGaleria {
 
     static retornaGaleriaComFiltroTextoInput(galeria: titulo[], filtroTextoInput: string = ''){
 
-      return (filtroTextoInput === '' ) ?
-          galeria
-          : galeria.filter(titulo => titulo.id.toString() == Utils.normalizarTexto(filtroTextoInput) || Utils.normalizarTexto(titulo.title).includes(Utils.normalizarTexto(filtroTextoInput)));
+      const textoNormalizado = Utils.normalizarTexto(filtroTextoInput);
+
+      if(!textoNormalizado || 'ALBUM '.includes(textoNormalizado))
+        return galeria;
+
+
+      const galeriaFilterId = galeria.filter(titulo => titulo.id.toString() == textoNormalizado);
+      const galeriaFilterAlbumId = galeria.filter(titulo => titulo.albumId.toString() == (textoNormalizado.replace('ALBUM ', '')));
+      const galeriaFilterTitle = galeria.filter(titulo => Utils.normalizarTexto(titulo.title).includes(textoNormalizado));
+      let galeriaSemDuplicatas = [...galeriaFilterId, ...galeriaFilterAlbumId , ...galeriaFilterTitle].filter((valorAtual, index, array) => {
+        return index === array.findIndex(valor => valor.id === valorAtual.id);
+      });
+
+      return galeriaSemDuplicatas;
     }
 }
